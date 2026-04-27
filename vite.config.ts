d771552d00b0bf0317df4a1a1d95d1756f75398c
Vivek <vivek@vivekmind.com>
2026-04-27 15:57:37 +0530

    fix cors

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         '/api': {
-          target: env.VITE_API_BASE_URL || 'http://localhost:3000',
+          target: env.VITE_PROXY_TARGET || 'http://localhost:3000',
           changeOrigin: true,
         }
       }
